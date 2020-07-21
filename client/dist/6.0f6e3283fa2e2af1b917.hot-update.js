d5b3c903d398cb9c783d7e399a0ab16916319edf
houssemguesmi <houssemguesmi14@gmail.com>
2020-07-21 11:22:34 +0100

    test

---
+++ client/dist/6.0f6e3283fa2e2af1b917.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate(6,{
+
+/***/ "./node_modules/cache-loader/dist/cjs.js?!./node_modules/babel-loader/lib/index.js!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/administrator/components/currentPatientsAdmin/currentPatientsList.vue?vue&type=script&lang=js&":
+false,
+
+/***/ "./node_modules/cache-loader/dist/cjs.js?{\"cacheDirectory\":\"node_modules/.cache/vue-loader\",\"cacheIdentifier\":\"5c29e566-vue-loader-template\"}!./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/administrator/components/currentPatientsAdmin/currentPatientsList.vue?vue&type=template&id=3ec3ddcc&scoped=true&":
+false,
+
+/***/ "./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/administrator/components/currentPatientsAdmin/currentPatientsList.vue?vue&type=style&index=0&id=3ec3ddcc&scoped=true&lang=css&":
+false,
+
+/***/ "./node_modules/vue-style-loader/index.js?!./node_modules/css-loader/index.js?!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/cache-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/administrator/components/currentPatientsAdmin/currentPatientsList.vue?vue&type=style&index=0&id=3ec3ddcc&scoped=true&lang=css&":
+false,
+
+/***/ "./src/administrator/components/currentPatientsAdmin/currentPatientsList.vue":
+false,
+
+/***/ "./src/administrator/components/currentPatientsAdmin/currentPatientsList.vue?vue&type=script&lang=js&":
+false,
+
+/***/ "./src/administrator/components/currentPatientsAdmin/currentPatientsList.vue?vue&type=style&index=0&id=3ec3ddcc&scoped=true&lang=css&":
+false,
+
+/***/ "./src/administrator/components/currentPatientsAdmin/currentPatientsList.vue?vue&type=template&id=3ec3ddcc&scoped=true&":
+false,
+
+/***/ "./src/assets/images/users/ali.jpg":
+false,
+
+/***/ "./src/assets/images/users/khaled.jpg":
+false
+
+})
