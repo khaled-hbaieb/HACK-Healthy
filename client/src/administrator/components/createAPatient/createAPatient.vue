bb0aaa14e9fab90b664caeb9f2c9f5d153b85e10
houssemguesmi <houssemguesmi14@gmail.com>
2020-07-24 21:02:02 +0100

    deploy test

--- client/src/administrator/components/createAPatient/createAPatient.vue
+++ client/src/administrator/components/createAPatient/createAPatient.vue
@@ -49,7 +49,7 @@
             <div class="centerx">
               <vs-upload
                 automatic
-                action="/upload-images"
+                action="http://localhost:3000/upload-images"
                 fileName="image"
                 @on-success="onFileUploaded"
               />
