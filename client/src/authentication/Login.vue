d4687a2b2906f6cc1fb9eceb8c310e7c5b837023
khaled-hbaieb <khaledhbaieb@yahoo.fr>
2020-08-04 17:19:42 +0100

    design login

--- client/src/authentication/Login.vue
+++ client/src/authentication/Login.vue
@@ -1,103 +1,133 @@
 <template>
-  <vs-col id="login-page-container" vs-lg="6">
-    <vs-card vs-lg="12" vs-xs="12">
-      <center>
-        <h3 class="text-center m-b-20">Sign In</h3>
-        <br />
-        <label class="col-md-12" for="example-email">Email</label>
-        <ValidationProvider rules="required|email">
-          <span slot-scope="{ errors }">
-            <vs-input
-              icon="account_circle"
-              class="login-page-inputs"
-              required
-              placeholder="E-mail"
-              v-model="email"
-              type="email"
-            />
-            <p>{{ errors[0] }}</p>
-          </span>
-        </ValidationProvider>
+  <div>
+    <vs-row>
+      <vs-col vs-lg="9"></vs-col>
+      <vs-col id="login-page-container" vs-lg="3">
+        <vs-card vs-lg="12" vs-xs="12">
+          <center>
+            <vs-row>
+              <vs-col vs-lg="12" v-if="!showRecover" id="loginform">
+                <h3 class="text-center m-b-20">Sign In</h3>
+                <br />
+                <label class="col-md-12" for="example-email">Email</label>
+                <ValidationProvider rules="required|email">
+                  <span slot-scope="{ errors }">
+                    <vs-input
+                      icon="account_circle"
+                      class="login-page-inputs"
+                      required
+                      placeholder="E-mail"
+                      v-model="email"
+                      type="email"
+                    />
+                    <p>{{ errors[0] }}</p>
+                  </span>
+                </ValidationProvider>
 
-        <ValidationProvider rules="required" v-slot="{ errors }">
-          <vs-input
-            dark
-            class="login-page-inputs"
-            type="password"
-            required
-            label-placeholder="Password"
-            v-model="password"
-            icon="lock"
-          />
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
+                <ValidationProvider rules="required" v-slot="{ errors }">
+                  <vs-input
+                    dark
+                    class="login-page-inputs"
+                    type="password"
+                    required
+                    label-placeholder="Password"
+                    v-model="password"
+                    icon="lock"
+                  />
+                  <span>{{ errors[0] }}</span>
+                </ValidationProvider>
 
-<vs-select icon="account_circle" placeholder="Select Your Role" v-model="userRole">
-          <vs-select-item text="Patient" value="patient"></vs-select-item>
-          <vs-select-item text="Doctor" value="doctor"></vs-select-item>
-          <vs-select-item text="Administrator" value="administrator"></vs-select-item>
-        </vs-select>
-        <div>
-         
-          <br />
-          <vs-button
-            id="btn-login"
-            class="login-page-buttons"
-            @click="handleLogin"
-            >Log In</vs-button
-          >
-        </div>
-        <h3>Recover Password</h3>
-        <p class="text-muted">
-          Enter your Email and instructions will be sent to you!
-        </p>
-        <vs-select
-          class="login-page-inputs"
-          type="text"
-          required
-          placeholder="Select Your Role"
-          v-model="recoveryRole"
-        >
-          <vs-select-item text="Patient" value="Patient"></vs-select-item>
-          <vs-select-item text="Doctor" value="Doctor"></vs-select-item>
-        </vs-select>
-        <vs-input
-          class="login-page-inputs"
-          type="text"
-          required
-          placeholder="Email"
-          v-model="recoveryEmail"
-          v-if="recoveryRole !== ''"
-        />
-        <vs-input
-          v-if="
-            recoveryEmail.includes('@') &&
-              recoveryEmail.includes('.') &&
-              recoveryEmail.length > '8'
-          "
-          class="login-page-inputs"
-          type="number"
-          required
-          placeholder="CIN"
-          v-model="recoveryCIN"
-        />
-        <vs-input
-          v-if="recoveryCIN.length === 8"
-          class="login-page-inputs"
-          type="number"
-          required
-          placeholder="Phone Number"
-          v-model="recoveryPhone"
-        />
-        <vs-button
-          v-if="recoveryPhone.length === 8"
-          class="login-page-buttons"
-          @click="sendEmail"
-          >Reset</vs-button
-        >
-      </center>
-    </vs-card>
-  </vs-col>
+                <vs-select
+                  icon="account_circle"
+                  placeholder="Select Your Role"
+                  v-model="userRole"
+                >
+                  <vs-select-item
+                    text="Patient"
+                    value="patient"
+                  ></vs-select-item>
+                  <vs-select-item text="Doctor" value="doctor"></vs-select-item>
+                  <vs-select-item
+                    text="Administrator"
+                    value="administrator"
+                  ></vs-select-item>
+                </vs-select>
+                <div>
+                  <br />
+                  <vs-button
+                    id="btn-login"
+                    class="login-page-buttons"
+                    @click="handleLogin"
+                    >Log In</vs-button
+                  >
+                </div>
+                <div class="ml-auto">
+                  <a
+                    icon="lock"
+                    id="to-recover"
+                    class="text-muted"
+                    @click="recover"
+                  >
+                    Forgot pwd?</a
+                  >
+                </div>
+              </vs-col>
+            </vs-row>
+            <div id="recoverform" v-if="showRecover">
+              <h3>Recover Password</h3>
+              <p class="text-muted">
+                Enter your Email and instructions will be sent to you!
+              </p>
+              <vs-select
+                class="login-page-inputs"
+                type="text"
+                required
+                placeholder="Select Your Role"
+                v-model="recoveryRole"
+              >
+                <vs-select-item text="Patient" value="Patient"></vs-select-item>
+                <vs-select-item text="Doctor" value="Doctor"></vs-select-item>
+              </vs-select>
+              <vs-input
+                class="login-page-inputs"
+                type="text"
+                required
+                placeholder="Email"
+                v-model="recoveryEmail"
+                v-if="recoveryRole !== ''"
+              />
+              <vs-input
+                v-if="
+                  recoveryEmail.includes('@') &&
+                    recoveryEmail.includes('.') &&
+                    recoveryEmail.length > '8'
+                "
+                class="login-page-inputs"
+                type="number"
+                required
+                placeholder="CIN"
+                v-model="recoveryCIN"
+              />
+              <vs-input
+                v-if="recoveryCIN.length === 8"
+                class="login-page-inputs"
+                type="number"
+                required
+                placeholder="Phone Number"
+                v-model="recoveryPhone"
+              />
+              <vs-button
+                v-if="recoveryPhone.length === 8"
+                class="login-page-buttons"
+                @click="sendEmail"
+                >Reset</vs-button
+              >
+            </div>
+          </center>
+        </vs-card>
+      </vs-col>
+    </vs-row>
+  </div>
 </template>
 <script>
 import doctor from "../models/doctor";
@@ -106,6 +136,7 @@ import patient from "../models/patient";
 import { ValidationProvider, extend } from "vee-validate";
 import { required, email } from "vee-validate/dist/rules";
 import axios from "axios";
+import $ from "jquery";
 extend("email", {
   ...email,
   message: "This field must be a valid email",
@@ -119,6 +150,10 @@ export default {
     ValidationProvider,
   },
   name: "Login",
+  beforeMount() {
+    console.log("mounted");
+    $("#recoverform").hide();
+  },
   data() {
     return {
       role: "",
@@ -130,8 +165,8 @@ export default {
       recoveryCIN: "",
       recoveryPhone: "",
       hasVisiblePassword: false,
-      userRole: '',
-
+      userRole: "",
+      showRecover: false,
     };
   },
   computed: {
@@ -140,21 +175,20 @@ export default {
     },
   },
   methods: {
-   
     handleLogin() {
       let user;
       this.loading = true;
 
       if (this.userRole === "doctor") {
-        console.log('doc')
+        console.log("doc");
         user = new doctor(this.email, this.password);
       }
       if (this.userRole === "administrator") {
         user = new administrator(this.email, this.password);
-        console.log('admi')
+        console.log("admi");
       }
       if (this.userRole === "patient") {
-        console.log('pat')
+        console.log("pat");
         user = new patient(this.email, this.password);
       }
       user.role = this.userRole;
@@ -186,18 +220,36 @@ export default {
       };
       let user = await axios.post(`/send`, inputs);
     },
+    recover() {
+      this.showRecover = true;
+      $("#loginform").slideUp();
+      $("#recoverform").fadeIn();
+    },
+    // rec() {
+    //   if (this.showRecover === false) {
+    //     $("#recoverform").hide();
+    //   }
+    // },
   },
 };
 </script>
-<style scoped>
+<style>
 #login-page-container {
-  margin-left: 22% !important;
-  margin-top: 15% !important;
+  /* margin-left: 83% !important; */
+  /* margin-top: 10% !important; */
+  /* width: auto !important; */
+  height: 120%;
+  padding: 0px !important;
 }
 .login-page-inputs {
   margin-bottom: 10px;
 }
 .login-page-buttons {
   margin-bottom: 10px;
+}
+body {
+  background-image: url("../assets/images/background/login3.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
