2dfa40a5fe5d7e525a9c763e3af022ab6fd0b235
aliSnakesou <lkollasfbt@gmail.com>
2020-08-07 20:07:29 +0100

    available rooms in clinic state charts working

--- client/src/administrator/components/clinicState/States.vue
+++ client/src/administrator/components/clinicState/States.vue
@@ -1,23 +1,35 @@
 <template>
   <vs-row>
-    <vs-col vs-lg="6" vs-xs="12">
+    <vs-col vs-lg="12" vs-xs="12">
       <vs-card>
-        <h3 class="mb-1 font-weight-normal">86%</h3>
-        <span class="text-muted">Available Rooms</span>
-        <vs-progress :percent="86" color="success">success</vs-progress>
+        <h3 class="mb-1 font-weight-normal">{{availableRooms(number)}}%</h3>
+        <span class="text-muted"><h1>Available Rooms</h1></span>
+        <vs-progress :percent="availableRooms(number)" color="success">success</vs-progress>
       </vs-card>
     </vs-col>
-    <vs-col vs-lg="6" vs-xs="12">
-      <vs-card>
-        <h3 class="mb-1 font-weight-normal">40%</h3>
-        <span class="text-muted">Available Doctors</span>
-        <vs-progress :percent="40" color="primary">primary</vs-progress>
-      </vs-card>
-    </vs-col>
+   
   </vs-row>
 </template>
 <script>
+import axios from "axios";
 export default {
-  name: "States"
+  name: "States",
+  data() {
+    return {
+      number: "",
+    };
+  },
+  methods:{
+    availableRooms(num){
+      return ((num/250)*100)
+    }
+  },
+  async beforeMount() {
+  let available =  await axios.post("/api/clinicX/rooms", {
+        availibility: true,
+      });
+      
+      this.number = available.data.length
+  }
 };
 </script>
