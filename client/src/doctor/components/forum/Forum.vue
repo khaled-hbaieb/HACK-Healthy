fc135fc5f31a7168421632ea3459c04c616a0fca
aliSnakesou <lkollasfbt@gmail.com>
2020-08-07 17:40:58 +0100

    moments js done in forum

--- client/src/doctor/components/forum/Forum.vue
+++ client/src/doctor/components/forum/Forum.vue
@@ -13,25 +13,14 @@
               type="button"
               class="btn btn-info d-block d-lg-block"
             >
-              <vs-icon
-                icon="add_circle_outline"
-                id="doctor-icon"
-                round
-              ></vs-icon
-              >Create New Post
+              <vs-icon icon="add_circle_outline" id="doctor-icon" round></vs-icon>Create New Post
             </vs-button>
           </div>
         </vs-col>
       </vs-row>
     </vs-card>
     <vs-row class="row page-titles">
-      <vs-col
-        vs-lg="3"
-        vs-sm="6"
-        vs-xs="12"
-        v-for="(post, index) in postsList"
-        :key="index"
-      >
+      <vs-col vs-lg="3" vs-sm="6" vs-xs="12" v-for="(post, index) in postsList" :key="index">
         <vs-card vs-lg="12" actionable>
           <div slot="header">
             <h5 class="font-weight-bold">{{ post.title }}</h5>
@@ -41,25 +30,24 @@
           </div>
           <div class="mb-2">
             <h6 class="mb-2">
-              {{ post.createdAt }}
+              {{
+              created[index]
+              }}
             </h6>
             <hr />
-            <vs-col vs-lg="9"
-              ><span class="py-2 d-inline">{{
+            <vs-col vs-lg="9">
+              <span class="py-2 d-inline">
+                {{
                 post.nameOfPoster
-              }}</span></vs-col
-            >
-            <vs-col vs-lg="3"> {{ post.views }} Views </vs-col>
+                }}
+              </span>
+            </vs-col>
+            <vs-col vs-lg="3">{{ post.views }} Views</vs-col>
           </div>
           <vs-row>
             <vs-col vs-lg="12">
-            <hr />
-              <vs-button
-                color="primary"
-                type="border"
-                @click="renderPost(post._id)"
-                >Read More</vs-button
-              >
+              <hr />
+              <vs-button color="primary" type="border" @click="renderPost(post._id)">Read More</vs-button>
             </vs-col>
           </vs-row>
         </vs-card>
@@ -69,18 +57,33 @@
 </template>
 <script>
 import axios from "axios";
+import moment from "moment";
 export default {
   name: "forum",
   data: () => {
     return {
       postsList: [],
       ready: false,
+      displayed: false,
+      created: {},
+      created2: {},
     };
   },
   async beforeMount() {
     let posts = await axios.get("/api/posts");
     this.postsList = posts.data;
+    for (let i = 0; i < this.postsList.length; i++) {
+      this.created[i] = this.postsList[i].createdAt;
+      this.created2[i] = this.postsList[i].createdAt;
+    }
+    this.getmomentDate();
     this.ready = true;
+  },
+  async updated() {
+    let posts = await axios.get("/api/posts");
+    this.postsList = posts.data;
+    // this.displayed = false
+    // this.getmomentDate()
   },
   computed: {
     isPatient() {
@@ -89,8 +92,36 @@ export default {
   },
   methods: {
     renderPost(arg) {
-      this.$router.push(`/doctor/forum/${arg}`);
+      if (localStorage.role == "patient") {
+        this.$router.push(`/patient/forum/${arg}`);
+      } else {
+        this.$router.push(`/doctor/forum/${arg}`);
+      }
     },
+    getmomentDate() {
+      if (this.displayed) {
+        setInterval(() => {
+          for (var key in this.created) {
+            this.created[key] = moment(
+              this.created2[key],
+              "YYYYMMDDhhmmss"
+            ).fromNow();
+          }
+        }, 1000);
+      } else {
+        for (var key in this.created) {
+          this.created[key] = moment(
+            this.created2[key],
+            "YYYYMMDDhhmmss"
+          ).fromNow();
+        }
+        this.displayed = true;
+        setTimeout(() => {
+          this.getmomentDate();
+        }, 2000);
+      }
+    },
+
     renderCreatePost() {
       this.$router.push(`/patient/forum/createpost`);
     },
--- client/src/doctor/components/post/Post.vue
+++ client/src/doctor/components/post/Post.vue
@@ -82,6 +82,7 @@
   </div>
 </template>
 <script>
+import UserService from '../../../services/user.service'
 import axios from "axios";
 export default {
   name: "post",
@@ -95,9 +96,19 @@ export default {
     };
   },
   async beforeMount() {
-    let post = await axios.post("/api/posts/findPost", {
-      _id: window.location.pathname.slice(14),
-    });
+    let user, post;
+    if (localStorage.role == "patient") {
+      user = await UserService.getPatientBoard();
+      post = await axios.post("/api/posts/findPost", {
+        _id: window.location.pathname.slice(15),
+      });
+    } else {
+      user = await UserService.getDoctorBoard();
+      post = await axios.post("/api/posts/findPost", {
+        _id: window.location.pathname.slice(14),
+      });
+    }
+    this.currentUser = user;
     this.post = post.data[0];
     let comments = await axios.post("/api/comments/findComments", {
       idOfPost: this.post._id,
