6d53c87bb6704da55bce0deca4397998133ac2bb
aliSnakesou <lkollasfbt@gmail.com>
2020-08-10 10:35:27 +0100

    change chart

--- client/src/administrator/components/clinicState/States.vue
+++ client/src/administrator/components/clinicState/States.vue
@@ -3,11 +3,12 @@
     <vs-col vs-lg="12" vs-xs="12">
       <vs-card>
         <h1 class="mb-1 font-weight-normal">{{availableRooms(number)}}%</h1>
-        <span class="text-muted"><h1>Available Rooms</h1></span>
+        <span class="text-muted">
+          <h1>Available Rooms</h1>
+        </span>
         <vs-progress :percent="availableRooms(number)" color="success">success</vs-progress>
       </vs-card>
     </vs-col>
-   
   </vs-row>
 </template>
 <script>
@@ -19,17 +20,17 @@ export default {
       number: "",
     };
   },
-  methods:{
-    availableRooms(num){
-      return ((num/250)*100)
-    }
+  methods: {
+    availableRooms(num) {
+      return ((num / 250) * 100).toFixed(2);
+    },
   },
   async beforeMount() {
-  let available =  await axios.post("/api/clinicX/rooms", {
-        availibility: true,
-      });
-      
-      this.number = available.data.length
-  }
+    let available = await axios.post("/api/clinicX/rooms", {
+      availibility: true,
+    });
+
+    this.number = available.data.length;
+  },
 };
 </script>
